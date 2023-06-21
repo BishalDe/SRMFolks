4e550c3276a9befc251b77fe78095b8399950282
Bishal De <itsbishalde@gmail.com>
2023-06-21 10:28:38 +0530

    re-creating files.!

---
+++ SRMFolks/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from SRMFolks import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homePage, name='homePage'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
